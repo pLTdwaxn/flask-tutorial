af2ac7b51416ec6971a21807b670b5141e50aa9b
LK <dontforgit@outlook.com>
2024-06-13 12:49:13 +0930

    Add index and create endpoints

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -29,9 +29,8 @@ def create_app(test_config=None):
     from . import auth
     app.register_blueprint(auth.bp)
 
-    # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
     
     return app
